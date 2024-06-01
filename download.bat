908e510b3dd24823d4d7592ea229e9d44df34da3
Bogyi2024 <sdserver600@gmail.com>
2024-06-01 20:44:56 +0630

    Create download.bat

---
+++ download.bat
@@ -0,0 +1,4 @@
+@echo off
+pip install gdown
+gdown https://drive.google.com/file/d/1uGJ2psd2M3GH72NHAFkxA9kqyDbWKdtu/view?usp=drive_link
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
