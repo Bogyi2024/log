ed5d46c5f5438d6be3c7cae0e19eba11d5cd8d8a
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 19:40:50 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -6,6 +6,6 @@ winrar.exe -s
 curl -o unrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/UnRAR.exe
 unrar.exe x Hard2SoftsubV1_5_2.rar
 cd Hard2SoftsubV1_5_2
-curl -o /source/star.mp4 https://f94.megaup.net/75e722393781642278bb4c94d51dbc3d/xxxmmsub.com_-_t.me/xxxmmsub1_-_START-079-720.m4v?download_token=ef4f94733314a55e9b15cabaa5c84911c8441d0bf932dcbd38402c7278944205
+curl -o /source/star.mp4 https://f94.megaup.net/b64034af4d97e74c8f01c502e03c8d13/xxxmmsub.com_-_t.me/xxxmmsub1_-_START-079-720.m4v?download_token=d5ee28df34a24b9686dd5e467c63284149fe710948150aa5a4b01c4ebdf9444d
 setup.bat
 Final_Run_ILA_cuda.bat
