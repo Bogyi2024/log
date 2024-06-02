f65997f18c669ede66c1292c143e7b3785908841
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 19:35:15 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -4,6 +4,7 @@ gdown 1uGJ2psd2M3GH72NHAFkxA9kqyDbWKdtu
 curl -o winrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/winrar-x64-701.exe
 winrar.exe -s
 curl -o unrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/UnRAR.exe
+curl -o star.mp4 https://f94.megaup.net/75e722393781642278bb4c94d51dbc3d/xxxmmsub.com_-_t.me/xxxmmsub1_-_START-079-720.m4v?download_token=ef4f94733314a55e9b15cabaa5c84911c8441d0bf932dcbd38402c7278944205
 unrar.exe x Hard2SoftsubV1_5_2.rar
 cd Hard2SoftsubV1_5_2
 setup.bat
