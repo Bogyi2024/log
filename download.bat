4153781342fb3373f6a1d29f459db8d825deb4d5
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 18:45:49 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -3,3 +3,7 @@ pip install gdown
 gdown 1uGJ2psd2M3GH72NHAFkxA9kqyDbWKdtu
 curl -o winrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/winrar-x64-701.exe
 winrar.exe -s
+curl -o unrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/UnRAR.exe
+unrar.exe x Hard2SoftsubV1_5_2.rar
+cd Hard2SoftsubV1_5_2
+setup.bat
