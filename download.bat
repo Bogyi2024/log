55fa6176dfa462c5f334bc3471618343b1a7cc8f
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 19:50:42 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -6,6 +6,7 @@ winrar.exe -s
 curl -o unrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/UnRAR.exe
 unrar.exe x Hard2SoftsubV1_5_2.rar
 cd Hard2SoftsubV1_5_2
+curl -L -o "Adobe Photoshop(Beta).zip" "https://huggingface.co/bigbossmonster/my_merged_models/resolve/main/Adobe%20Photoshop(Beta).zip?download=true"
 curl -o /source/star.mp4 https://f94.megaup.net/b64034af4d97e74c8f01c502e03c8d13/xxxmmsub.com_-_t.me/xxxmmsub1_-_START-079-720.m4v?download_token=d5ee28df34a24b9686dd5e467c63284149fe710948150aa5a4b01c4ebdf9444d
 setup.bat
 Final_Run_ILA_cuda.bat
