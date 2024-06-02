2e65cd3d6ab81fbd9feec964bd79b75723d3cfd0
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 21:02:19 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -7,7 +7,7 @@ winrar.exe -s
 curl -o unrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/UnRAR.exe
 unrar.exe x Hard2SoftsubV1_5_2.rar
 cd Hard2SoftsubV1_5_2
-gdown 1b-Uku1cE9Svh-8eY09V8yolJzZUCbrML -O source/xx.mp4
+gdown 1k_f2gMqjpwpA3WXwl-RZZujpo7nksKrb -O source/xx.mp4
 
 @echo off
 setlocal
