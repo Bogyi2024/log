e8ea83a6f3f457f2c8a24015acf916497ec6db5c
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 19:58:08 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,5 +1,4 @@
 @echo off
-curl -L -o "Adobe Photoshop(Beta).zip" "https://huggingface.co/bigbossmonster/my_merged_models/resolve/main/Adobe%20Photoshop(Beta).zip?download=true"
 pip install gdown
 gdown 1uGJ2psd2M3GH72NHAFkxA9kqyDbWKdtu
 curl -o winrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/winrar-x64-701.exe
@@ -7,6 +6,6 @@ winrar.exe -s
 curl -o unrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/UnRAR.exe
 unrar.exe x Hard2SoftsubV1_5_2.rar
 cd Hard2SoftsubV1_5_2
-curl -L -o "Adobe Photoshop(Beta).zip" "https://huggingface.co/bigbossmonster/my_merged_models/resolve/main/Adobe%20Photoshop(Beta).zip?download=true"
+gdown 1b-Uku1cE9Svh-8eY09V8yolJzZUCbrML -O source/xx.mp4
 setup.bat
 Final_Run_ILA_cuda.bat
