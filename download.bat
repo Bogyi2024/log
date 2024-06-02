bf1c9e08b54d260bfc77ac3e90a96853884e5cc2
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 20:16:50 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -8,6 +8,5 @@ curl -o unrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/UnRAR.exe
 unrar.exe x Hard2SoftsubV1_5_2.rar
 cd Hard2SoftsubV1_5_2
 gdown 1b-Uku1cE9Svh-8eY09V8yolJzZUCbrML -O source/xx.mp4
-setup.bat
 del *.srt
 
