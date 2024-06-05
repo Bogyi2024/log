0fc4353764f976b3be7f4172e521537a216cc433
Bogyi2024 <sdserver600@gmail.com>
2024-06-05 23:51:35 +0630

    Update katfile.py

--- dep_download/katfile.py
+++ dep_download/katfile.py
@@ -1,48 +1,49 @@
 import os
-import re
 import requests
 import subprocess
 
-single_line_batch_links = "https://katfile.com/wa7vuxtwbe9u/415169_3xplanet_Uncen-leaked_YSN-365.part1.rar.html" # @param {type:"string"}
+single_line_batch_links = "https://katfile.com/0to3nid6r6k9/415167_3xplanet_Uncen-leaked_WANZ-894.part1.rar.html"  # @param {type:"string"}
 
-# Splitting the link by space might not be necessary since it contains only one link
-# perlink = single_line_batch_links.split(" ")
+perlink = single_line_batch_links.split(" ")
 
-# if single_line_batch_links != "":
-#     files = perlink
-# else:
-#     file_url1 = "" # @param {type:"string"}
-#     file_url2 = "" # @param {type:"string"}
-#     file_url3 = "" # @param {type:"string"}
-#     file_url4 = "" # @param {type:"string"}
-#     file_url5 = "" # @param {type:"string"}
-#     files = [file_url1, file_url2, file_url3, file_url4, file_url5]
+if single_line_batch_links != "":
+    files = perlink
 
-files = [single_line_batch_links]  # Using the provided link
+os.mkdir("download")
 
 for url in files:
     if url == "":
         continue
     else:
-        output_path = "download/" # @param {type:"string"}
+        output_path = "download/"  # @param {type:"string"}
         apiKey = "699996yph6h88a7rc6c1g8"
         parts = url.split('/')
         domain = parts[2]
         filecode = parts[3]
-
+        
         cloneurl = f"https://{domain}/api/file/clone?key={apiKey}&file_code={filecode}"
         response = requests.get(cloneurl)
+        
         if response.status_code == 200:
             json_data = response.json()
             download_url = json_data.get('result', {}).get('url')
+            
             parts_final = download_url.split('/')
             filecodex = parts_final[3]
             final_url = f"https://{domain}/api/file/direct_link?key={apiKey}&file_code={filecodex}"
-            response = requests.get(final_url, stream=True)
+            response = requests.get(final_url)
+            
             if response.status_code == 200:
-                filename = os.path.join(output_path, download_url.split('/')[-1])
-                with open(filename, 'wb') as f:
-                    for chunk in response.iter_content(chunk_size=8192):
-                        f.write(chunk)
+                json_data = response.json()
+                download_url = json_data.get('result', {}).get('url')
+                
+                if download_url:
+                    # Construct the command for curl
+                    local_filename = os.path.join(output_path, download_url.split('/')[-1])
+                    command = ['curl', '-o', local_filename, download_url]
+                    subprocess.run(command, check=True)
+                    print(f"Downloaded: {local_filename}")
+            else:
+                print("Error while fetching direct link from Katfile API")
         else:
-            print("Error while fetching Katfile API")
+            print("Error while fetching clone link from Katfile API")
