5b9dd4bb59fe2e8a00da9d728619a58b5513f869
Bogyi2024 <sdserver600@gmail.com>
2024-06-09 18:29:30 +0630

    Update cm720.py

--- cm_download/cm720.py
+++ cm_download/cm720.py
@@ -1,6 +1,8 @@
 import os
+import re
 import requests
 import gdown
+import subprocess
 
 def fetch_links_from_pastebin(pastebin_link):
     response = requests.get(pastebin_link)
@@ -9,6 +11,16 @@ def fetch_links_from_pastebin(pastebin_link):
     else:
         print(f"Error fetching from Pastebin: {response.status_code}")
         return []
+
+
+def extract_google_drive_id(url):
+    drive_id_pattern = re.compile(r'(?:drive.google.com/.*?id=|drive.google.com/file/d/|drive.google.com/open\?id=|drive.google.com/uc\?id=)([a-zA-Z0-9_-]{33,})')
+    match = drive_id_pattern.search(url)
+    if match:
+        return match.group(1)
+    else:
+        print(f"Invalid Google Drive URL: {url}")
+        return None
 
 pastebin_link = "https://pastebin.com/raw/p2Xk0N0b"
 
@@ -19,11 +31,12 @@ if single_line_batch_links:
     os.makedirs(output_path, exist_ok=True)
 
     for url in single_line_batch_links:
-        if "drive.google.com" in url:
+        file_id = extract_google_drive_id(url)
+        if file_id:
             try:
-                gdown.download(url=url, output=output_path, fuzzy=True)
+                subprocess.run(['gdown', file_id, '-O', output_path], check=True)
                 print(f"Downloaded: {url}")
-            except Exception as e:
+            except subprocess.CalledProcessError as e:
                 print(f"Error downloading from Google Drive: {str(e)}")
         else:
             if url:
@@ -39,3 +52,4 @@ if single_line_batch_links:
                 print("Empty URL encountered")
 else:
     print("No links found in the Pastebin link.")
+  
