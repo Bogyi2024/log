16f794ed1c1d2ff43cb53f0aa455dac3a608b187
Bogyi2024 <sdserver600@gmail.com>
2024-06-09 16:54:41 +0630

    Update cm720.py

--- cm_download/cm720.py
+++ cm_download/cm720.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import gdown
 
 def fetch_links_from_pastebin(pastebin_link):
     response = requests.get(pastebin_link)
@@ -18,13 +19,23 @@ if single_line_batch_links:
     os.makedirs(output_path, exist_ok=True)
 
     for url in single_line_batch_links:
-        if url:
-            filename = url.split('/')[-1]
-            filepath = os.path.join(output_path, filename)
+        if "drive.google.com" in url:
             try:
-                os.system(f"aria2c.exe -x 16 -d {output_path} {url}")
-                print(f"Downloaded: {filename}")
+                gdown.download(url=url, output=output_path, fuzzy=True)
+                print(f"Downloaded: {url}")
             except Exception as e:
-                print(f"Error downloading {filename}: {str(e)}")
+                print(f"Error downloading from Google Drive: {str(e)}")
         else:
-            print("Empty URL encountered")
+            if url:
+                filename = url.split('/')[-1]
+                filepath = os.path.join(output_path, filename)
+                try:
+                    # Ensure aria2c is installed and in the system's PATH
+                    os.system(f"aria2c.exe -x 16 -d {output_path} {url}")
+                    print(f"Downloaded: {filename}")
+                except Exception as e:
+                    print(f"Error downloading {filename}: {str(e)}")
+            else:
+                print("Empty URL encountered")
+else:
+    print("No links found in the Pastebin link.")
