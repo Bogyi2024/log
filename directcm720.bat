dfe46255f9feed4c557f1bc452b5faeb8cf10758
Bogyi2024 <sdserver600@gmail.com>
2024-06-09 18:47:02 +0630

    Update directcm720.bat

--- directcm720.bat
+++ directcm720.bat
@@ -1,6 +1,7 @@
 @echo off
 rem Installing
 pip install gdown
+pip install --upgrade --no-cache-dir gdown
 gdown 1q421vbzHgXv7cG7quC6YVQi4iW1ASvOY
 curl -o winrar.exe https://raw.githubusercontent.com/Bogyi2024/log/main/winrar-x64-701.exe
 winrar.exe -s
