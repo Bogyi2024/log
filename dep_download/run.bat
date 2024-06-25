7a1b0e6633c699f7ed95ddc81c46513655ad4126
Bogyi2024 <sdserver600@gmail.com>
2024-06-25 11:49:16 +0630

    Update run.bat

--- dep_download/run.bat
+++ dep_download/run.bat
@@ -1,5 +1,6 @@
 pip install requests
 pip install huggingface-hub
+pin install gdown
 curl -o katfile.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/katfile.py
 python katfile.py
 curl -o huggingface.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/huggingface.py
