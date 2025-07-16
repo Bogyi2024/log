37259486114d28c943fdf94bf9204b2258ca0efc
Bogyi2024 <sdserver1@gmail.com>
2025-07-16 23:57:09 +0630

    Update rpgl_ml.sh

--- dep_download/rpgl_ml.sh
+++ dep_download/rpgl_ml.sh
@@ -6,8 +6,8 @@ pip install requests
 pip install huggingface-hub
 
 # Download rapidgator.py and run it
-curl -o rapidgator.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/rapidgator.py
-python rapidgator.py
+curl -o rapidgator.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/rpgl_ml.py
+python rpgl_ml.py
 
 # Download huggingface.py and run it
 curl -o huggingface.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/huggingface.py
