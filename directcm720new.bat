c9757a0539fac2a6c5ea38ed0b3364df32227292
Bogyi2024 <sdserver1@gmail.com>
2025-10-27 12:42:27 +0630

    Update directcm720new.bat

--- directcm720new.bat
+++ directcm720new.bat
@@ -3,6 +3,7 @@ rem Installing
 pip install gdown
 pip install yagmail
 pip install chardet
+pip install git+https://github.com/monsterhunters/srtmerge.git
 pip install huggingface_hub
 pip install --upgrade --no-cache-dir gdown
 pip install git+https://github.com/Juvenal-Yescas/mediafire-dl
