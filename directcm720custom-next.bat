b45cf6f6a4b046c154f5c6639631d217e4960c17
Bogyi2024 <sdserver1@gmail.com>
2025-11-08 21:22:46 +0630

    Update directcm720custom-next.bat

--- directcm720custom-next.bat
+++ directcm720custom-next.bat
@@ -12,8 +12,8 @@ unrar.exe x Hard2SoftsubV1_5_2.rar
 cd Hard2SoftsubV1_5_2
 curl -o send_email_file.py https://raw.githubusercontent.com/Bogyi2024/log/main/send_email_file.py
 curl -o aria2c.exe https://raw.githubusercontent.com/Bogyi2024/log/main/aria2c.exe
-curl -o cm720.py https://raw.githubusercontent.com/Bogyi2024/log/main/cm_download/cm720.py
-python cm720.py
+curl -o cm720-next.py https://raw.githubusercontent.com/Bogyi2024/log/main/cm_download/cm720-next.py
+python cm720-next.py
 curl -o core/settings/general.cfg %SETTING%
 
 @echo off
