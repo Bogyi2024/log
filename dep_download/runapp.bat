c134aadd2e558855c77025412163f40d75bccf4e
Bogyi2024 <sdserver1@gmail.com>
2025-11-14 17:26:09 +0630

    Create runapp.bat

---
+++ dep_download/runapp.bat
@@ -0,0 +1,9 @@
+pip install requests
+pip install huggingface-hub
+pip install gdown
+pip install --upgrade gdown
+pip install git+https://github.com/Juvenal-Yescas/mediafire-dl
+curl -o katfileapp.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/katfileapp.py
+python katfileapp.py
+curl -o huggingface.py https://raw.githubusercontent.com/Bogyi2024/log/main/dep_download/huggingface.py
+python huggingface.py
